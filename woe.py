6c1c26e6b40012b69d247fdb1ae1a484eecc19d8
Rodolfo <73193435+RodolfoRbl@users.noreply.github.com>
2024-02-07 14:54:04 -0600

    changes to save and transform

--- woe.py
+++ woe.py
@@ -1,11 +1,3 @@
-import config_pc
-import math
-import json
-from pyspark.sql import DataFrame
-from pyspark.sql import functions as F
-from pyspark.sql.functions import col
-
-
 class WOE_IV:
     
     def __init__(self, df: DataFrame, cols_to_woe: [str], label_column: str, good_label: int):
@@ -14,13 +6,13 @@ class WOE_IV:
         self.label_column = label_column
         self.good_label = good_label
         self.fit_data = {}
-
     def fit(self):
         agg_class = self.df.groupby((col(self.label_column) == self.good_label).alias('ix_good_label')).count().cache()
         total_good = agg_class.filter(F.col('ix_good_label') == 'true').select('count').collect()[0][0]
         total_bad = agg_class.filter(F.col('ix_good_label') == 'false').select('count').collect()[0][0]
+        print(f'Good: {total_good}')
+        print(f'Bad: {total_bad}')
         agg_class.unpersist()
-
         for col_to_woe in self.cols_to_woe:
             print(f'Trabajando en categoria {col_to_woe}')
             ag_col = self.df.withColumn('ix_good_label',(col(self.label_column) == self.good_label))\
@@ -28,22 +20,23 @@ class WOE_IV:
             
             categories = ag_col.select(col_to_woe).collect()
             categories = [i[0] for i in categories]
-
-            for cat_value in categories:
-                row_cat_label = ag_col.filter((col(col_to_woe) == cat_value)).collect()[0]
+            for pos, cat_value in enumerate(categories):
+                print(f'Categoria {cat_value}, {pos} de {len(categories)}')
+                if cat_value != None:
+                    row_cat_label = ag_col.filter((col(col_to_woe) == cat_value)).collect()[0] 
+                else:
+                    row_cat_label = ag_col.filter((col(col_to_woe).isNull())).collect()[0] 
                 
                 good_amount = row_cat_label['true']
                 bad_amount = row_cat_label['false']
-
                 good_amount = good_amount if good_amount != 0 else 0.5
                 bad_amount = bad_amount if bad_amount != 0 else 0.5
                 
                 good_dist = good_amount / total_good
                 bad_dist = bad_amount / total_bad
-
                 self.build_fit_data(col_to_woe, cat_value, good_dist, bad_dist)
-
             ag_col.unpersist()
+
 
     def build_fit_data(self, col_to_woe, category, good_dist, bad_dist):
         woe_final = math.log(good_dist / bad_dist)
@@ -58,16 +51,18 @@ class WOE_IV:
         with open(path,'w') as file:
             json.dump(self.fit_data, file)
         print('Datos guardados')
-
     @classmethod
-    def transform(self, df: DataFrame, drop_original = False):
-        for col_to_woe, woe_info in self.fit_data.items():
-            itera_woe_info =  F.coalesce(*[F.when(F.col(col_to_woe) == category, F.lit(woe_iv['woe'])) for category, woe_iv in woe_info.items()],F.lit(0))
+    def transform(self, df: DataFrame, drop_original = False, only_consider = []):
+        '''transformar df'''
+        temp_dic = {key: self.fit_data[key] for key in self.fit_data if key in only_consider} if only_consider else self.fit_data        
+        for col_to_woe, woe_info in temp_dic.items():
+            itera_woe_info =  F.coalesce(*[F.when(F.col(col_to_woe) == category, F.lit(woe_iv['woe'])) for category, woe_iv in woe_info.items()],
+                                         F.lit(woe_info['null']['woe']),
+                                         F.lit(0))
             df = df.withColumn(col_to_woe + '_woe', itera_woe_info)
         if drop_original:
             return df.drop(*self.fit_data.keys())
         return df
-
     @classmethod
     def load_params(self, path):
         with open(path,'r') as file:
